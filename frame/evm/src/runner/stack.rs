9e9ec14dabdeea2f938a8e5c57f904b1b22a5f3d
Grigoriy Simonov <gsimonov@usetech.com>
2023-07-05 19:42:43 +0000

    feat: make OnChargeEvmTransaction aware of user action

--- frame/evm/src/runner/stack.rs
+++ frame/evm/src/runner/stack.rs
@@ -54,6 +54,7 @@ use fp_evm::{
 
 // Unique
 use crate::{account::CrossAccountId, CurrentLogs};
+use fp_evm::WithdrawReason;
 
 #[cfg(feature = "forbid-evm-reentrancy")]
 environmental::thread_local_impl!(static IN_EVM: environmental::RefCell<bool> = environmental::RefCell::new(false));
@@ -78,6 +79,7 @@ where
 		gas_limit: u64,
 		max_fee_per_gas: Option<U256>,
 		max_priority_fee_per_gas: Option<U256>,
+		reason: WithdrawReason,
 		config: &'config evm::Config,
 		precompiles: &'precompiles T::PrecompilesType,
 		is_transactional: bool,
@@ -112,6 +114,7 @@ where
 			gas_limit,
 			max_fee_per_gas,
 			max_priority_fee_per_gas,
+			reason,
 			config,
 			precompiles,
 			is_transactional,
@@ -140,6 +143,7 @@ where
 		mut gas_limit: u64,
 		max_fee_per_gas: Option<U256>,
 		max_priority_fee_per_gas: Option<U256>,
+		reason: WithdrawReason,
 		config: &'config evm::Config,
 		precompiles: &'precompiles T::PrecompilesType,
 		is_transactional: bool,
@@ -244,7 +248,7 @@ where
 				})?;
 
 		// Deduct fee from the `source` account. Returns `None` if `total_fee` is Zero.
-		let fee = T::OnChargeTransaction::withdraw_fee(&source, total_fee)
+		let fee = T::OnChargeTransaction::withdraw_fee(&source, reason, total_fee)
 			.map_err(|e| RunnerError { error: e, weight })?;
 
 		// Execute the EVM call.
@@ -457,6 +461,10 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CallInfo, RunnerError<Self::Error>> {
+		let reason = WithdrawReason::Call {
+			target,
+			input: input.clone(),
+		};
 		if validate {
 			Self::validate(
 				source.clone(),
@@ -481,6 +489,7 @@ where
 			gas_limit,
 			max_fee_per_gas,
 			max_priority_fee_per_gas,
+			reason,
 			config,
 			&precompiles,
 			is_transactional,
@@ -517,6 +526,7 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let reason = WithdrawReason::Create;
 		if validate {
 			Self::validate(
 				source.clone(),
@@ -541,6 +551,7 @@ where
 			gas_limit,
 			max_fee_per_gas,
 			max_priority_fee_per_gas,
+			reason,
 			config,
 			&precompiles,
 			is_transactional,
@@ -577,6 +588,7 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let reason = WithdrawReason::Create2;
 		if validate {
 			Self::validate(
 				source.clone(),
@@ -602,6 +614,7 @@ where
 			gas_limit,
 			max_fee_per_gas,
 			max_priority_fee_per_gas,
+			reason,
 			config,
 			&precompiles,
 			is_transactional,
