fd54ed219ad93995d7743f0e2cbb98795e76204d
Grigoriy Simonov <gsimonov@usetech.com>
2023-07-07 07:26:09 +0000

    feat: extended precompile interface

--- frame/ethereum/src/mock.rs
+++ frame/ethereum/src/mock.rs
@@ -200,6 +200,7 @@ impl pallet_evm::Config for Test {
 	// Unique:
 	type CrossAccountId = CrossAccountId<Self>;
 	type BackwardsAddressMapping = HashedAddressMapping;
+	type OnMethodCall = ();
 }
 
 parameter_types! {
--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -348,6 +348,7 @@ impl pallet_evm::Config for Runtime {
 	// Unique:
 	type CrossAccountId = Self::AccountId;
 	type BackwardsAddressMapping = IdentityAddressMapping;
+	type OnMethodCall = ();
 }
 
 parameter_types! {
