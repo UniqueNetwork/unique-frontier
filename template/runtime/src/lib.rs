d7d165680c9323ebee99fe1ef187c9ad9a01a39c
Grigoriy Simonov <gsimonov@usetech.com>
2023-07-18 06:09:51 +0000

    feat: extended precompile interface

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -349,6 +349,7 @@ impl pallet_evm::Config for Runtime {
 	// Unique:
 	type CrossAccountId = Self::AccountId;
 	type BackwardsAddressMapping = IdentityAddressMapping;
+	type OnMethodCall = ();
 }
 
 parameter_types! {
